2d293a6c9658134c84a17a20d122f61fa63e365d
Adam Faulconbridge <adam@sanogenetics.com>
2020-08-19 13:01:54 +0100

    move to next dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="puretabix",
-    version="1.0.2",
+    version="1.0.3-dev",
     author="Adam Faulconbridge",
     author_email="afaulconbridge@googlemail.com",
     packages=["puretabix"],
