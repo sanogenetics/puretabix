2d8684c9d0667db65b4c28abcfdf6986cd1775f3
Adam Faulconbridge <adam@sanogenetics.com>
2021-12-01 15:42:50 +0000

    extra convenience import

--- puretabix/__init__.py
+++ puretabix/__init__.py
@@ -1,3 +1,8 @@
 from . import bgzip  # noqa: disable=F401
 from .tabix import TabixIndex, TabixIndexedFile  # noqa: disable=F401
-from .vcf import VCFAccumulator, VCFLine, get_vcf_fsm  # noqa: disable=F401
+from .vcf import (  # noqa: disable=F401
+    LINE_START,
+    VCFAccumulator,
+    VCFLine,
+    get_vcf_fsm,
+)
