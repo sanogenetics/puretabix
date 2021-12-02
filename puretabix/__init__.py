73c7c6310d6b87a4a2a85896e728a7ba66da9eff
Adam Faulconbridge <adam@sanogenetics.com>
2021-12-02 13:27:40 +0000

    extra imports

--- puretabix/__init__.py
+++ puretabix/__init__.py
@@ -1,4 +1,6 @@
 from . import bgzip  # noqa: disable=F401
+from .bgzip import BlockGZipReader, BlockGZipWriter  # noqa: disable=F401
+from .mp import MultiprocessGeneratorPool  # noqa: disable=F401
 from .tabix import TabixIndex, TabixIndexedFile  # noqa: disable=F401
 from .vcf import (  # noqa: disable=F401
     LINE_START,
